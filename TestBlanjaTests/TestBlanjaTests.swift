1c569b83776a0b1e9b8a6a533aa3ee4b3dbdb67c
Fajar <fajar@codigo.id>
2018-08-27 12:00:48 +0700

    Initial Commit

---
+++ TestBlanjaTests/TestBlanjaTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestBlanjaTests.swift
+//  TestBlanjaTests
+//
+//  Created by Fajar on 8/27/18.
+//  Copyright © 2018 Fajar. All rights reserved.
+//
+
+import XCTest
+@testable import TestBlanja
+
+class TestBlanjaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
